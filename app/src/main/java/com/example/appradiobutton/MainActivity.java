89c688ecc01102a6d7dde29bd4a38d0c0c4fe4fd
Juan Pablo <juan.saucedo@axity.com>
2022-03-23 09:15:21 -0600

    Se termina el ejercicio 15

--- app/src/main/java/com/example/appradiobutton/MainActivity.java
+++ app/src/main/java/com/example/appradiobutton/MainActivity.java
@@ -7,12 +7,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText et1, et2;
     private TextView tv1;
-    private RadioButton rb1, rb2;
+    private RadioButton rb1, rb2, rb3, rb4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         tv1 = (TextView) findViewById(R.id.txtResult);
         rb1 = (RadioButton) findViewById(R.id.rbSumar);
         rb2 = (RadioButton) findViewById(R.id.rbRestar);
+        rb3 = (RadioButton) findViewById(R.id.rbMultiplicar);
+        rb4 = (RadioButton) findViewById(R.id.rbDividir);
     }
 
     //Metodo para el boton calcular
@@ -33,15 +36,22 @@ public class MainActivity extends AppCompatActivity {
 
         int valor1Int = Integer.parseInt(valor1String);
         int valor2Int = Integer.parseInt(valor2String);
+        int result = 0;
+        String resultado;
 
         if (rb1.isChecked()){
-            int suma = valor1Int + valor2Int;
-            String resultado = String.valueOf(suma);
-            tv1.setText(resultado);
+            result = valor1Int + valor2Int;
         } else if (rb2.isChecked()){
-            int resta = valor1Int - valor2Int;
-            String resultado = String.valueOf(resta);
-            tv1.setText(resultado);
+            result = valor1Int - valor2Int;
+        } else if (rb3.isChecked()){
+            result = valor1Int * valor2Int;
+        } else if (rb4.isChecked() && valor2Int != 0){
+            result = valor1Int / valor2Int;
+        } else if (valor2Int == 0 && rb4.isChecked()){
+            Toast.makeText(this, "No se puede dividir entre cero", Toast.LENGTH_LONG).show();
         }
+
+        resultado = String.valueOf(result);
+        tv1.setText(resultado);
     }
 }
